5a83ed3bc4558c0ede8f66f2fef0756d05afc712
bagdonas <k.bagdonavicius@gmail.com>
2023-06-08 18:15:55 +0300

    host *

--- conference_app/conference_app/settings.py
+++ conference_app/conference_app/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
